2fabe6ab9da699aed8a3cac03ea5139ac76291f4
Dumra <d.shuliakov@gmail.com>
2015-04-29 04:03:28 -0700

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -1,6 +1,6 @@
 <?php
 
-      define("CURRENCY", "http://www.webservicex.net/CurrencyConvertor.asmx?WSDL");
-      define("CITIES", "http://www.webservicex.net/globalweather.asmx?WSDL");
+      define("CURRENCY", "http://www.webservicex.net/CurrencyConvertor.asmx");
+      define("CITIES", "http://www.webservicex.net/globalweather.asmx");
 
 ?>
