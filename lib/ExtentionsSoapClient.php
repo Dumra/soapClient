55ba6b2a0f78bd95f186ab0f71001ded4930ee07
Dumra <d.shuliakov@gmail.com>
2015-04-29 04:01:21 -0700

    Update ExtentionsSoapClient.php

--- lib/ExtentionsSoapClient.php
+++ lib/ExtentionsSoapClient.php
@@ -5,14 +5,14 @@ class ExtentionsSoapClient {
 
 	public function getCurrencyConversionResult($currency, $url)
 	{
-		$client = new SoapClient($url);
+		$client = new SoapClient($url . '?WSDL');
 		$obj = $client->ConversionRate($currency);
 		return $obj->ConversionRateResult;
 	}
 
 	public function getCities($country, $url)
 	{
-		$client = new SoapClient($url);
+		$client = new SoapClient($url . '?WSDL');
 		$obj = $client->GetCitiesByCountry($country);
 		$xml = $obj->GetCitiesByCountryResult;
 		return $xml;
