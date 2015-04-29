5f0381beb853d605de2291c753b8dcb4cdfe061f
Dumra <d.shuliakov@gmail.com>
2015-04-29 04:02:55 -0700

    Update CurlSoapClient.php

--- lib/CurlSoapClient.php
+++ lib/CurlSoapClient.php
@@ -5,17 +5,17 @@ class CurlSoapClient {
 
 	private $ch;
 
-	public function getCurrencyConversionResult($from, $to)
+	public function getCurrencyConversionResult($from, $to, $url)
 	{
-		$ch = curl_init("http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=$from&ToCurrency=$to");
+		$ch = curl_init($url . "/ConversionRate?FromCurrency=$from&ToCurrency=$to");
 		$this->ch = $ch;
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		return curl_exec($ch);
 	}
 
-	public function getCities($country)
+	public function getCities($country, , $url)
 	{
-		$ch = curl_init("http://www.webservicex.net//globalweather.asmx/GetCitiesByCountry?CountryName=$country");
+		$ch = curl_init($url . "/GetCitiesByCountry?CountryName=$country");
 		$this->ch = $ch;
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$str = simplexml_load_string(curl_exec($ch));
